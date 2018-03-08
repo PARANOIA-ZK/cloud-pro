4f6a4052c84dfb29c577fa2827a222f735377afd
designinn <paranoia_zk@yeah.net>
2018-03-08 17:55:06 +0800

    使用分布式服务追踪 sleuth + zipkin

--- eureka/client/consumer/feign-consumer/src/main/java/com/consumer/FeignConsumerApplication.java
+++ eureka/client/consumer/feign-consumer/src/main/java/com/consumer/FeignConsumerApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignConsumerApplication {
+
+    //zipkin跟踪策略，全部跟踪 -- 412
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeignConsumerApplication.class, args);
--- eureka/client/service/service-demo/src/main/java/com/service/ServiceDemoApplication.java
+++ eureka/client/service/service-demo/src/main/java/com/service/ServiceDemoApplication.java
@@ -3,10 +3,18 @@ package com.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class ServiceDemoApplication {
+
+    //zipkin跟踪策略，全部跟踪 -- 412
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDemoApplication.class, args);
